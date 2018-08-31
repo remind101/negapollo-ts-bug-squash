a375560c8ca473aa6f333e9e36ab5ea9f716bbcc
Sophie Dawson <sophie@remind101.com>
2018-08-31 11:02:44 -0700

    Add health and proxy graphql endpoints

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   overrides: [
     {
       files: ['**/*.ts'],
-      parse: 'typescript-eslint-parser',
+      parser: 'typescript-eslint-parser',
       plugins: ['typescript'],
       rules: {
         'no-undef': 'off',
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,50 @@
+import _ from 'lodash';
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import proxy from 'proxy-middleware';
 import rollbar from 'lib/rollbar';
+import url from 'url';
 
 const GRAPHQL_PORT = process.env.PORT || 8080;
+
+const apolloUrl = url.parse(process.env.APOLLO_PROXY_URL || '');
+const cookieDomain = process.env.APOLLO_PROXY_COOKIE_DOMAIN || '';
+const env = process.env.NODE_ENV || 'no-env';
 
 function initApp() {
   const app = express();
 
-  app.get('/', (req, res) => res.send('Hello world'));
   app.use(rollbar.errorHandler());
+  app.use(cookieParser());
 
-  // TODO: add health endpoint
-
-  app.use('/boom', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  app.get('/boom', (req: express.Request, res: express.Response) => {
     rollbar.error('Test error');
-    res.end();
-    next();
+    res.status(500).send('Just a test error');
   });
+
+  app.get('/health', (req: express.Request, res: express.Response) => {
+    res.sendStatus(200).end();
+  });
+
+  app.use(
+    '/graphql',
+    proxy(
+      Object.assign({}, apolloUrl, {
+        cookieRewrite: cookieDomain,
+        pathname: '/graphql',
+      }),
+    ),
+  );
+
+  app.use(
+    '/graphiql',
+    proxy(
+      Object.assign({}, apolloUrl, {
+        cookieRewrite: cookieDomain,
+        pathname: '/graphiql',
+      }),
+    ),
+  );
 
   app.use((req: express.Request, res: express.Response) => {
     res.status(404).send('Not Found');
@@ -26,4 +55,8 @@ function initApp() {
 
 const app = initApp();
 
-app.listen(GRAPHQL_PORT, () => console.log(`blah`));
+app.listen(GRAPHQL_PORT, () =>
+  console.log(
+    'Negapollo proxy server is now running on http://localhost:' + GRAPHQL_PORT + ' as ' + env,
+  ),
+);
