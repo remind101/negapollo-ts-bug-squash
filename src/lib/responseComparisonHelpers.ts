3baf23c092e244aa37c3f5292d84a1f6440d6067
Sophie Dawson <sophiecdawson@gmail.com>
2018-10-09 15:37:59 -0700

    Normalize avatarUrl and profilePictureUrls for diffing (#10)
    * Add custom comparator to filter cdn urls
    
    * Add full cdn comparison
    
    * Build comparator func
    
    * Yarn format fixes

--- src/lib/responseComparisonHelpers.ts
+++ src/lib/responseComparisonHelpers.ts
@@ -1,4 +1,5 @@
-import { isEqual } from 'lodash';
+import { includes, isEqualWith } from 'lodash';
+import normalizeUrl from 'normalize-url';
 import rollbar from 'lib/rollbar';
 import statsd from 'lib/statsd';
 
@@ -14,10 +15,24 @@ export default function logGraphqlResHitMissMismatch(
     rollbar.debug(`${datadogKey} mismatch between new and old results`, { oldResult, newResult });
   } else if (!newResult && oldResult) {
     statsd.increment(`${datadogKey}.miss`);
-  } else if (isEqual(newResult, oldResult)) {
+  } else if (isEqualWith(newResult, oldResult, createComparator())) {
     statsd.increment(`${datadogKey}.match`);
   } else {
     statsd.increment(`${datadogKey}.mismatch`);
     rollbar.debug(`${datadogKey} mismatch between new and old results`, { oldResult, newResult });
   }
 }
+
+const createComparator = (): ((f: any, s: any, k?: any) => boolean | void) => {
+  const urlFieldsToNormalize = ['profilePictureUrl', 'avatarUrl'];
+  return (first: any, second: any, key?: any): boolean | void => {
+    if (typeof first === 'string' && typeof second === 'string') {
+      if (key && includes(urlFieldsToNormalize, key)) {
+        const firstCdn = normalizeUrl(first);
+        const secondCdn = normalizeUrl(second);
+        return firstCdn === secondCdn;
+      }
+    }
+    return undefined;
+  };
+};
